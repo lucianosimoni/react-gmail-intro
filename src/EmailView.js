7fb095ea6fd7f9271642004d13bb9d72bb12ab96
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-03 16:37:41 +0000

    Fix on CSS bug in Header
    Once Header has an element with the class .left-menu and the component
    LeftMenu also has a class .left-menu, LeftMenu class was overlaping
    Header and breaking it up. To fix it:
    - Added row and column position to Header > .left-menu, and also removed
    the padding of it.

--- src/EmailView.js
+++ src/EmailView.js
@@ -11,80 +11,80 @@ function EmailView () {
     return (
         <main className="email-view">
             <nav className="email-toolbar">
-            <ul>
-                <li>
-                <img className="icon" src={backArrow} alt="reply button" />
-                </li>
-                <li>
-                <img className="icon" src={downloadButton} alt="archive button" />
-                </li>
-                <li>
-                <img className="icon" src={rubbishButton} alt="delete button" />
-                </li>
-            </ul>
-
-            <div className="space"></div>
-            <div>
-                <p>1 of 25</p>
-                <button>&lt;</button>
-                <button>&gt;</button>
-            </div>
-            </nav>
-
-            <article className="email-content">
-            <div className="title">
-                <h1>Welcome to Flaticon</h1>
-            </div>
-
-            <header className="email-content--header">
-                <div className="avatar"></div>
-                <div className="email-info">
-                <div className="sender-info">
-                    <h2>Freepik Company</h2>
-                    <em>&lt;no-reply@freepik.com&gt;</em>
-                </div>
-                <div className="user-info">
-                    <p>
-                    to me <em>&lt;nicolas@boolean.co.uk&gt;</em>
-                    </p>
-                </div>
-                </div>
-                <div className="date-info">
-                <p>17 March 2021, 09:33</p>
-                </div>
-                <div className="email-action-icons">
                 <ul>
                     <li>
                     <img className="icon" src={backArrow} alt="reply button" />
                     </li>
                     <li>
-                    <img
-                        className="icon"
-                        src={rateStarButton}
-                        alt="star button"
-                    />
+                    <img className="icon" src={downloadButton} alt="archive button" />
                     </li>
                     <li>
-                    <img
-                        className="icon"
-                        src={rubbishButton}
-                        alt="delete button"
-                    />
+                    <img className="icon" src={rubbishButton} alt="delete button" />
                     </li>
                 </ul>
+
+                <div className="space"></div>
+                <div>
+                    <p>1 of 25</p>
+                    <button>&lt;</button>
+                    <button>&gt;</button>
                 </div>
-            </header>
-            
-            <section className="email-body">
-                <div className="content">
-                <img src={flaticonWelcomeImage} alt="Flaticon welcome message" />
+            </nav>
+
+            <article className="email-content">
+                <div className="title">
+                    <h1>Welcome to Flaticon</h1>
                 </div>
-            </section>
-            
-            <section className="email-actions">
-                <button>Reply</button>
-                <button>Forward</button>
-            </section>
+
+                <header className="email-content--header">
+                    <div className="avatar"></div>
+                    <div className="email-info">
+                    <div className="sender-info">
+                        <h2>Freepik Company</h2>
+                        <em>&lt;no-reply@freepik.com&gt;</em>
+                    </div>
+                    <div className="user-info">
+                        <p>
+                        to me <em>&lt;nicolas@boolean.co.uk&gt;</em>
+                        </p>
+                    </div>
+                    </div>
+                    <div className="date-info">
+                    <p>17 March 2021, 09:33</p>
+                    </div>
+                    <div className="email-action-icons">
+                    <ul>
+                        <li>
+                        <img className="icon" src={backArrow} alt="reply button" />
+                        </li>
+                        <li>
+                        <img
+                            className="icon"
+                            src={rateStarButton}
+                            alt="star button"
+                        />
+                        </li>
+                        <li>
+                        <img
+                            className="icon"
+                            src={rubbishButton}
+                            alt="delete button"
+                        />
+                        </li>
+                    </ul>
+                    </div>
+                </header>
+                
+                <section className="email-body">
+                    <div className="content">
+                    <img src={flaticonWelcomeImage} alt="Flaticon welcome message" />
+                    </div>
+                </section>
+                
+                <section className="email-actions">
+                    <button>Reply</button>
+                    <button>Forward</button>
+                </section>
             </article>
         </main>
     )
